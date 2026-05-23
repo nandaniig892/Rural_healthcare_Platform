d449218bd718311274b86363cde5a33776c72451
Nandani Gupta <nandanigupta892@gmail.com>
2026-05-23 20:59:02 +0530

    Add files via upload

---
+++ RuralHealthcareApplication.java
@@ -0,0 +1,14 @@
+package com.ruralhealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class RuralHealthcareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuralHealthcareApplication.class, args);
+    }
+}
